bb2fb94e57ccf4ec59f519dc96dcdc315fa570e5
Cyfer <130665260+cyffeer@users.noreply.github.com>
2024-10-18 15:12:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var name = "Tom";
+    var age = 24;
+    var skills = ["HTML", "CSS", "JS"];
+    var skillsMember = {
+        name: name,
+        age: age,
+        skills: skills
+    };
+    return skillsMember;
+}
